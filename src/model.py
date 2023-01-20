feb41db85b2576840369b705d25eaaf2e75c2f25
mobtgzhang <mobtgzhang@outlook.com>
2023-01-20 21:09:47 +0800

    modify the codes

--- src/model.py
+++ src/model.py
@@ -19,10 +19,6 @@ class KGTConv(nn.Module):
         self.num_ents = config.num_ents
         self.num_rels = config.num_rels
         self.emb_dim = config.emb_dim
-        self.k_fa = config.k_fa
-        self.fa_rho = config.fa_rho
-        self.fa_alpha = config.fa_alpha
-        self.fa_dropout = config.fa_dropout
 
         self.k_pr = config.k_pr
         self.pr_alpha = config.pr_alpha
@@ -35,7 +31,6 @@ class KGTConv(nn.Module):
         # the feaures
         self.rg_feature = nn.Parameter(torch.Tensor(size=(config.num_ents,config.emb_dim)),requires_grad=True)
         self.ap_feature = nn.Parameter(torch.Tensor(size=(config.num_ents,config.emb_dim)),requires_grad=True)
-        self.fa_feature = nn.Parameter(torch.Tensor(size=(config.num_ents,config.emb_dim)),requires_grad=True)
         # the mlp layer
         mlp_emb = (config.emb_dim*2)*2+config.emb_dim
         self.fnn = FNNNet(mlp_emb,config.emb_dim,2)
@@ -58,7 +53,6 @@ class KGTConv(nn.Module):
         return F.softmax(logits)
     def graph_forward(self,x,edge_index,edge_type):
         x_emb = self.ent_emb(x)
-        self.fa_feature.data = self.fa_net(x_emb,edge_index)
         self.ap_feature.data = self.appnet(x_emb,edge_index)
         self.rg_feature.data = self.rgcn(x_emb,edge_index,edge_type)
         
